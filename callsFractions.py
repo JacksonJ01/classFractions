ee1dd39d038f9e1bb07f96fee181e73ed7ca1ef6
JacksonJ01 <jacksonj@hartwick.edu>
2019-11-13 10:48:25 -0500

    Add files via upload

---
+++ callsFractions.py
@@ -0,0 +1,5 @@
+from classFractions import Fraction
+
+f1 = Fraction(1, 2)
+f2 = Fraction(1, 4)
+
---
+++ classFractions.py
@@ -0,0 +1,10 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.num = numerator
+        self.den = denominator
+
+    def adding(self):
+        return 
+
+# answer_num = numA * denB + numB * denA
+# answer_den = denA * denB
